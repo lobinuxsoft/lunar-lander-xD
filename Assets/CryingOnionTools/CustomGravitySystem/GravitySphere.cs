a2564b3771545afbe25b5db89e3e3058532d65dd
Lobinux <lobinux@digilearnnials.com>
2022-05-03 19:59:10 -0300

    Make changes to SphereGravity

--- Assets/CryingOnionTools/CustomGravitySystem/GravitySphere.cs
+++ Assets/CryingOnionTools/CustomGravitySystem/GravitySphere.cs
@@ -10,15 +10,21 @@ namespace CryingOnionTools.GravitySystem
     {
         [SerializeField] float gravity = 9.81f;
 
-        [SerializeField, Min(0f)] float outerRadius = 10f, outerFalloffRadius = 15f;
+        [SerializeField, Min(0f)] private float innerFalloffRadius = 15f, innerRadius = 18f;
+        [SerializeField, Min(0f)] float outerRadius = 22f, outerFalloffRadius = 22f;
         
-        float outerFalloffFactor;
+        float innerFalloffFactor, outerFalloffFactor;
 
         void Awake() => OnValidate();
 
         void OnValidate ()
         {
+            innerFalloffRadius = Mathf.Max(innerFalloffRadius, 0f);
+            innerRadius = Mathf.Max(innerRadius, innerFalloffRadius);
+            outerRadius = Mathf.Max(outerRadius, innerRadius);
             outerFalloffRadius = Mathf.Max(outerFalloffRadius, outerRadius);
+            
+            innerFalloffFactor = 1f / (innerRadius - innerFalloffRadius);
             outerFalloffFactor = 1f / (outerFalloffRadius - outerRadius);
         }
 
@@ -27,11 +33,18 @@ namespace CryingOnionTools.GravitySystem
             Vector3 vector = transform.position - position;
             float distance = vector.magnitude;
             
-            if (distance > outerFalloffRadius) return Vector3.zero;
+            if (distance > outerFalloffRadius || distance < innerFalloffRadius) return Vector3.zero;
 
             float g = gravity / distance;
 
-            if (distance > outerRadius) g *= 1f - (distance - outerRadius) * outerFalloffFactor;
+            if (distance > outerRadius)
+            {
+                g *= 1f - (distance - outerRadius) * outerFalloffFactor;
+            }
+            else if (distance < innerRadius)
+            {
+                g *= 1f - (innerRadius - distance) * innerFalloffFactor;
+            }
             
             return g * vector;
         }
@@ -41,7 +54,19 @@ namespace CryingOnionTools.GravitySystem
         {
             Vector3 p = transform.position;
             
+            if (innerFalloffRadius > 0f && innerFalloffRadius < innerRadius) 
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawWireSphere(p, innerFalloffRadius);
+            }
+            
             Gizmos.color = Color.yellow;
+
+            if (innerRadius > 0 && innerRadius < outerRadius)
+            {
+                Gizmos.DrawWireSphere(p, innerRadius);
+            }
+            
             Gizmos.DrawWireSphere(p, outerRadius);
             Handles.color = new Color(Color.yellow.r, Color.yellow.g, Color.yellow.b, .15f); 
             Handles.SphereHandleCap(0, p, Quaternion.identity, outerRadius * 2, EventType.Repaint);
