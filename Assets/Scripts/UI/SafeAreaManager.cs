786c76a7d8c09117e559a1d93e61a4efd845a764
Lobinux <lobinux@digilearnnials.com>
2022-05-05 20:46:41 -0300

    Add safe area manager

---
+++ Assets/Scripts/UI/SafeAreaManager.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SafeAreaManager : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<SafeAreaManager, UxmlTraits> {}
+
+    public SafeAreaManager()
+    {
+        style.flexGrow = 1;
+        style.flexShrink = 1;
+        RegisterCallback<GeometryChangedEvent>(LayoutChanged);
+    }
+
+    private void LayoutChanged(GeometryChangedEvent e)
+    {
+        var safeArea = Screen.safeArea;
+
+        try
+        {
+            var leftTop = RuntimePanelUtils.ScreenToPanel(panel,new Vector2(safeArea.xMin, Screen.height - safeArea.yMax));
+            var rightBottom = RuntimePanelUtils.ScreenToPanel(panel,new Vector2(Screen.width - safeArea.xMax, safeArea.yMin));
+        
+            style.marginLeft = leftTop.x;
+            style.marginTop = leftTop.y;
+            style.marginRight = rightBottom.x;
+            style.marginBottom = rightBottom.y;
+        }
+        catch (InvalidCastException) {}
+    } 
+}
